09a8a3b59645d32b4b60664e62017cb3215d3bf3
Miko Chu <angelomycar@gmail.com>
2021-01-11 17:51:03 +0800

    added refresh token and renamed token to access token

--- app/JWT.php
+++ app/JWT.php
@@ -6,12 +6,24 @@ use Lcobucci\JWT\Token\Plain;
 
 interface JWT {
     /**
+     * Generate access token
+     * 
      * @param string id for user
      * @return Plain token
      */
-    function issueToken(string $id);
+    function issueAccessToken(string $id);
 
     /**
+     * Generate refresh token to request new access token
+     * 
+     * @return Plain token
+     * @return void
+     */
+    function issueRefreshToken();
+
+    /**
+     * Verify if token is valid
+     * 
      * @param Plain token
      * @return boolean <code>true</code> valid token;
      *                 <code>false</code> otherwise.
